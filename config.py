ada1fdbfda4dd3dcda99067c3f82522bab845b3a
Chris Valaas <cvalaas@mozilla.com>
2018-07-20 15:07:13 -0700

    adding a config.py sample

---
+++ config.py
@@ -0,0 +1,5 @@
+config = {
+  'proxies'      : {"https" : "http://proxy.dmz.scl3.mozilla.com:3128"}
+  'snippets_dir' : '/Users/cvalaas/git/data/snippets'
+  'geoip_db_loc' : '/Users/cvalaas/Downloads/GeoLite2-Country_20180605/GeoLite2-Country.mmdb'
+}
