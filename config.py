403a3ad6b76fdc7f561206d0d7f03033da61d6f3
Chris Valaas <cvalaas@mozilla.com>
2018-09-17 13:24:02 -0700

    updates

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 config = {
-  'proxies'            : {"https" : "http://proxy.dmz.scl3.mozilla.com:3128"},
-  'snippets_dir'       : '/Users/cvalaas/git/data/snippets-stats/haul_logs',
-  'geoip_db_loc'       : '/Users/cvalaas/Downloads/GeoLite2-Country_20180605/GeoLite2-Country.mmdb',
-  's3_snippets_bucket' : 'snippets_bucket_name_here',
-  's3_snippets_path'   : '/path/to/logs/on/s3',
+  'proxies'               : {"https" : "http://proxy.dmz.scl3.mozilla.com:3128"},
+  'snippets_dir'          : '/Users/cvalaas/git/data/snippets-stats/haul_logs',
+  'geoip_db_loc'          : '/Users/cvalaas/Downloads/GeoLite2-Country_20180605/GeoLite2-Country.mmdb',
+  's3_snippets_bucket'    : 'snippets_bucket_name_here',
+  's3_snippets_path'      : '/path/to/logs/on/s3',
+  's3_snippets_bucket_AK' : 'blah',
+  's3_snippets_bucket_SK' : 'blah',
 }
--- get_snippets_logs.py
+++ get_snippets_logs.py
@@ -2,7 +2,7 @@
 
 import time
 import sys,os,errno,re,argparse
-from datetime import datetime
+import datetime
 from subprocess import call
 from config import config
 
@@ -10,22 +10,36 @@ proxies = config['proxies']
 
 def print_debug(level, message):
   if debug >= level:
-    print("[%s] %s" % (datetime.now(),message))
+    print("[%s] %s" % (datetime.datetime.now(),message))
 
-def get_snippets_logs():
+def get_snippets_logs(fetch_date):
 
-  s3_uri = 's3://' + config['s3_snippets_bucket'] + config['s3_snippets_path']
-  call(['aws','s3','sync',s3_uri,config['snippets_dir'])
+  if not fetch_date:
+    fetch_date = datetime.datetime.now().strftime('%Y-%m-%d')
+    print_debug(3, "No date given. Using %s" % fetch_date)
+
+  fetch_date_l = [ i for i in fetch_date.split('-') ]
+  date_no_dashes = ''.join(fetch_date_l)
+
+  os.environ['AWS_ACCESS_KEY_ID']     = config['s3_snippets_bucket_AK']
+  os.environ['AWS_SECRET_ACCESS_KEY'] = config['s3_snippets_bucket_SK']
+
+  s3_uri = 's3://' + config['s3_snippets_bucket'] + config['s3_snippets_path'] + '/' + date_no_dashes
+  aws_command_l = ['aws', 's3', 'sync', s3_uri, os.path.join(s3_uri,config['snippets_dir'], fetch_date)]
+  print_debug(3, "Calling: %s" % ' '.join(aws_command_l))
+  call( aws_command_l )
 
 if __name__ == "__main__":
  
   parser = argparse.ArgumentParser(description="Get snippets-stats logs from S3")
   parser.add_argument('-d', '--debug', action='store', help='debug level', type=int, default=3)
-  #parser.add_argument('-f', '--force', action='store_true', help='force changes even if there are a lot')
+  parser.add_argument('--date', action='store', help='date to fetch', type=str, default='')
   args = parser.parse_args()
 
   debug = args.debug
 
   print_debug(1, "Starting...")
 
-  get_snippets_logs()
+  get_snippets_logs(args.date)
+
+  print_debug(1, "Finished.")
